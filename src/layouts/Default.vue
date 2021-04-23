56db7eaf5f4c0258ea8850ae0381c26cb730c153
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-04-23 21:56:40 +0530

    added header and footer components

--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
+  <div class="bg-light-50 dark:bg-blue-700">
+    <Header />
     <slot />
+    <Footer />
   </div>
 </template>
+
+<script>
+import Header from './partials/Header'
+import Footer from './partials/Footer'
+
+export default {
+  components: {
+    Header,
+    Footer
+  }
+}
+</script>
