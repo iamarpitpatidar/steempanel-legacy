f8ca93b0c79672f2bff25f8df1b361245d6aa2f6
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-04-23 22:23:41 +0530

    added logo

--- src/components/Logo.vue
+++ src/components/Logo.vue
@@ -1,3 +1,23 @@
 <template>
-  <div>Logo</div>
+  <g-link
+    class="flex-shrink-0 flex items-center"
+    to="/"
+    title="Back to home"
+    oncontextmenu="return false;"
+  >
+    <Logo class="hidden lg:block h-8 w-auto text-gray-800 dark:text-white" />
+    <LogoSmall class="block lg:hidden h-8 w-auto" />
+  </g-link>
 </template>
+
+<script>
+import Logo from '~/assets/images/steem-logo.svg'
+import LogoSmall from '~/assets/images/steem-logo-small.svg'
+
+export default {
+  components: {
+    Logo,
+    LogoSmall
+  }
+}
+</script>
