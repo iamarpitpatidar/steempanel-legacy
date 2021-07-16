a88918e3d62cb93b3cd9cb01f28c81a9785a67f2
Arpit Patidar <arpit.ptdr@hotmail.com>
2021-07-16 13:00:13 +0530

    new: added About page

---
+++ src/pages/About.vue
@@ -0,0 +1,50 @@
+<template>
+  <Layout>
+    <section class="px-4 md:px-8 py-8 space-y-4 text-gray-600 dark:text-gray-400">
+      <div class="pb-2">
+        <div class="font-rajdhani text-xl text-gray-700 dark:text-gray-350">What is Steem ?</div>
+        <div class="text-sm sm:text-base">Steem is a social media blockchain that allows and grows communities in the ecosystem and makes immediate revenue possible for users by rewarding them for sharing good and quality content. It’s the first platform on blockchain that can power real applications via social media apps like Steemit.</div>
+      </div>
+      <div class="pb-2">
+        <div class="font-rajdhani text-xl text-gray-700 dark:text-gray-350">What is steemPanel ?</div>
+        <div class="text-sm sm:text-base space-y-2">
+          <div class="">SteemPanel is the user friendly and beautifully designed blockchain explorer. It gives you all the information that you need to look for your steem account. It gives you detailed information regarding your account, vote value, curation rewards and most important thing is the APR (Annual Percentage Return) you are generating on your account. It is free to use application anyone can use it by just putting their account credentials and you will get all the information required.</div>
+          <div>
+            <div>Some Special features:</div>
+            <ul class="list-disc list-inside">
+              <li>Curation rewards calculator (Per Day, Week , Monthly)</li>
+              <li>APR % (Annual Percentage Return) on your account.</li>
+              <li>Vote Value (Vote Weight Calculator)</li>
+              <li>Latest STEEM, SBD, TRON (TRX) Price</li>
+              <li>News Letter</li>
+            </ul>
+          </div>
+          <div>Since we’ve started this steempanel, we have grown immensely and today we are helping new audience.our website is the largest information source for technincal & non technical newbies on teem blockchain– including news, latest updates regarding Steem.</div>
+          <div class="pt-4 font-rajdhani text-lg text-gray-700 dark:text-gray-350">Hope you will like and enjoy spending your time on Steempanel.</div>
+        </div>
+      </div>
+    </section>
+  </Layout>
+</template>
+<script>
+export default {
+  metaInfo: {
+    title: 'About Us',
+    link: [
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.googleapis.com'
+      },
+      {
+        rel: 'preconnect',
+        href: 'https://fonts.gstatic.com',
+        crossorigin: ''
+      },
+      {
+        rel: 'stylesheet',
+        href: 'https://fonts.googleapis.com/css2?family=Rajdhani:wght@600&display=swap'
+      }
+    ]
+  }
+}
+</script>
