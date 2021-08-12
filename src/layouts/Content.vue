79ac358f10d32551e2b1d2c7940fc5d47bf885c1
runebanks <admin@runebanks>
2021-08-12 01:41:09 -0700

    fixed layouts for small pages

--- src/layouts/Content.vue
+++ src/layouts/Content.vue
@@ -1,7 +1,7 @@
 <template>
-  <div class='bg-gray-100 dark:bg-gray-700'>
+  <div class='flex flex-col bg-gray-100 dark:bg-gray-700 min-h-screen'>
     <Header />
-    <main class="px-4 sm:px-6 lg:px-8 mt-16 py-4">
+    <main class='px-4 sm:px-6 lg:px-8 mt-2 py-4'>
       <slot />
     </main>
     <Footer />
--- src/layouts/Default.vue
+++ src/layouts/Default.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="bg-gray-100 dark:bg-gray-700">
+  <div class="flex flex-col bg-gray-100 dark:bg-gray-700 min-h-screen">
     <Header />
     <slot />
     <Footer />
