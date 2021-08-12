fb2443a9e28745552cc02ec07cc676ac37386615
runebanks <admin@runebanks>
2021-08-12 01:15:21 -0700

    Updated nav link

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -2,7 +2,7 @@
   <div class="hidden sm:block sm:ml-6">
     <div class="flex space-x-4">
       <nav-link
-        to="/news/"
+        to="/news"
         class="px-3 py-2 rounded-md text-sm font-medium"
       >
         News
